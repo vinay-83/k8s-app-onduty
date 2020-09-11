d5f9114343eb1094aeb71957df670dfbd015883d
vinay-83 <vennuvinay7@gmail.com>
2020-09-11 16:26:41 +0530

    first commit

---
+++ src/main/java/com/isg/od/K8sAppOndutyApplication.java
@@ -0,0 +1,13 @@
+package com.isg.od;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sAppOndutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sAppOndutyApplication.class, args);
+	}
+
+}
